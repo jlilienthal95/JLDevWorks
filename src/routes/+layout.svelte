100a570b3aa597444ad9dd33c7cced7bce203b14
joggest95 <jilienthal@gmail.com>
2025-02-18 13:12:37 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+
+	const linkClass = "hover:opacity-60"
+	const liClass = "lg:after:content-['|'] md:after:ml-4 last:after:content-none"
+</script>
+
+<div class="relative min-h-screen h-auto w-full bg-cover bg-blue-50 font-bold">
+	<div id="header" class="flex flex-row fixed w-full h-42 justify-between items-center lg:px-40 pr-12 z-100 bg-gradient-to-b to-transparent from-black/30 backdrop-blur-xs">
+		<div id="logo" class="h-full">
+			<img class="h-full max-h-full object-contain" src='/JLDevWorksLogo.png' alt="JL DevWorks"/>
+		</div>
+		<ul class="flex lg:flex-row flex-col md:gap-4" id="links">
+			<li class={liClass} id="about">
+				About
+			</li>
+			<li class={liClass} id="recentWork">
+				Recent Products
+			</li>
+			<li class={liClass} id="contact">
+				Contact
+			</li>
+		</ul>
+		<!-- <div id= "contactInfo" class="md:flex flex-col hidden">
+			<a href="tel:999999apps" class={linkClass}>
+				<div id="phone">
+					999-999-APPS
+				</div>
+			</a>
+			<a href="mailto:JLDevWorks@gmail.com" class={linkClass}>
+				<div id="email">
+					JLDevWorks@gmail.com
+				</div>
+			</a>
+		</div> -->
+	</div>
+	<div class="relative flex flex-col justify-center items-center pt-60 flex-1 px-6">
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	@font-face {
+    font-family: 'Jost';
+    src: url('/fonts/Jost-VariableFont_wght.ttf') format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    const lgTextCont = "md:w-2/3 w-full"
+    const lgTextClass = "text-[50px] md:text-[75px] lg:text-[90px] xl:text-[100px]"
+    const smTextClass = "text-[18px] sm:text-[32px] md:text-[41px] xl:text-[38px]"
+</script>
+
+<div class="flex flex-col flex-1 h-full justify-center items-center mt-4 gap-16">
+    <div class="flex md:flex-row flex-col w-full justify-start items-center">
+        <div id="headlineCont" class="flex flex-1 w-full">
+            <div class={lgTextCont}>
+                <div id="headline" class={lgTextClass}>
+                    BEAUTIFUL, RESPONSIVE WEB AND MOBILE APPLICATIONS.
+                </div>
+            </div>
+            <!-- <div class='flex flex-col md:gap-y-8 gap-y-2 text-2xl'>
+                <div class="text-4xl italic font-jost">
+                    Beautiful, responsive web & mobile applications.
+                </div>
+                <div>Need a skilled developer to bring your idea to life? Whether it’s a dynamic website, a high-performance mobile app, or a custom software solution, I can help.
+                    <br/><br/>
+                    I’m Joshua, a Full Stack Software Engineer specializing in modern, accessible web and application development. I build sleek, responsive frontends using React, Next.js, and Svelte, and craft scalable, high-performance backends with Node.js and Express. From intuitive user interfaces to rock-solid backend architecture, I ensure seamless, efficient, and reliable experiences across all platforms.
+                    <br/><br/>
+                    If you need a website that stands out, a mobile app that engages users, or a custom software solution tailored to your business, I’m here to help.
+                    <br/><br/>
+                    Let’s bring your vision to life. Reach out today!
+                </div>
+            </div> -->
+        </div>
+    </div>
+    <div class="flex lg:flex-row flex-col w-full md:justify-between items-end gap-20">
+        <div class="flex items-end text-[100px] underline">
+            JOSHUA<br>LILIENTHAL
+        </div>
+        <div id="photoContainer" class="flex">
+            <img class="object-contain rounded-4xl max-h-120" src="/profile pic.jpeg" alt="Joshua Lilienthal | Fullstack Engineer"/>
+        </div>
+    </div>
+    <div id="intersectionCont" class="flex flex-1 lg:flex-row flex-col w-full md:justify-start gap-4">        
+        <div class="flex flex-1 md:justify-start">
+            <div class={lgTextClass} id="gallery">
+                AT THE INTERSECTION OF TECHNOLOGY AND CREATIVITY—
+            </div>
+        </div>
+        <div id="buildingIntCont" class="xl:w-1/3 lg:w-1/3">
+            <div id="buildingInt" class={smTextClass}>
+                BUILDING INTUITIVE, ACCESSIBLE, AND HIGH-PERFORMANCE APPLICATIONS. WHETHER IT’S CRAFTING SLEEK UIS OR ARCHITECTING SCALABLE BACKENDS, I FOCUS ON CREATING SEAMLESS USER EXPERIENCES.
+            </div>
+        </div>
+    </div>
+</div>
