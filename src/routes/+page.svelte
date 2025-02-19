cf40b1e4d80f059a7b35d12bbbfd4351b19a05f9
joggest95 <jilienthal@gmail.com>
2025-02-19 12:26:23 -0500

    fixed responsiveness of homepage for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
     const lgTextCont = "md:w-2/3 w-full"
-    const lgTextClass = "text-[50px] md:text-[75px] lg:text-[90px] xl:text-[100px]"
-    const smTextClass = "text-[18px] sm:text-[32px] md:text-[41px] xl:text-[38px]"
+    const lgTextClass = "text-[50px] md:text-[75px] xl:text-[100px]"
+    const smTextClass = "text-[18px] sm:text-[32px] md:text-[38px] lg:text-[30px] xl:text-[38px]"
 </script>
 
-<div class="flex flex-col flex-1 h-full justify-center items-center mt-4 gap-16">
-    <div class="flex md:flex-row flex-col w-full justify-start items-center">
+<div class="flex flex-col flex-1 h-full w-full justify-center items-center mt-4 gap-1">
+    <div id="headlineFormat" class="flex md:flex-row flex-col w-full justify-start items-center">
         <div id="headlineCont" class="flex flex-1 w-full">
             <div class={lgTextCont}>
                 <div id="headline" class={lgTextClass}>
@@ -27,21 +27,21 @@
             </div> -->
         </div>
     </div>
-    <div class="flex lg:flex-row flex-col w-full md:justify-between items-end gap-20">
-        <div class="flex items-end text-[100px] underline">
+    <div id="joshuaCont" class="flex lg:flex-row flex-col w-full md:justify-between sm:items-end items-center gap-20">
+        <div class="flex sm:text-start text-end sm:text-[100px] text-[60px] underline">
             JOSHUA<br>LILIENTHAL
         </div>
         <div id="photoContainer" class="flex">
             <img class="object-contain rounded-4xl max-h-120" src="/profile pic.jpeg" alt="Joshua Lilienthal | Fullstack Engineer"/>
         </div>
     </div>
-    <div id="intersectionCont" class="flex flex-1 lg:flex-row flex-col w-full md:justify-start gap-4">        
+    <div id="intersectionCont" class="flex flex-1 lg:flex-row flex-col w-full gap-4">        
         <div class="flex flex-1 md:justify-start">
             <div class={lgTextClass} id="gallery">
-                AT THE INTERSECTION OF TECHNOLOGY AND CREATIVITY—
+                AT THE INTERSECTION OF TECHNOLOGY AND CREATIVITY&nbsp;—
             </div>
         </div>
-        <div id="buildingIntCont" class="xl:w-1/3 lg:w-1/3">
+        <div id="buildingIntCont" class="xl:w-1/3 lg:w-1/3 text-end">
             <div id="buildingInt" class={smTextClass}>
                 BUILDING INTUITIVE, ACCESSIBLE, AND HIGH-PERFORMANCE APPLICATIONS. WHETHER IT’S CRAFTING SLEEK UIS OR ARCHITECTING SCALABLE BACKENDS, I FOCUS ON CREATING SEAMLESS USER EXPERIENCES.
             </div>
