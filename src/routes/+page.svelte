00b283761fde535c551f8acd90586b6d9b802da2
joggest95 <jilienthal@gmail.com>
2025-02-22 11:33:17 -0500

    styling on nameCont

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,23 +37,10 @@
                     BEAUTIFUL, RESPONSIVE WEB AND MOBILE APPLICATIONS.
                 </div>
             </div>
-            <!-- <div class='flex flex-col md:gap-y-8 gap-y-2 text-2xl'>
-                <div class="text-4xl italic font-jost">
-                    Beautiful, responsive web & mobile applications.
-                </div>
-                <div>Need a skilled developer to bring your idea to life? Whether it’s a dynamic website, a high-performance mobile app, or a custom software solution, I can help.
-                    <br/><br/>
-                    I’m Joshua, a Full Stack Software Engineer specializing in modern, accessible web and application development. I build sleek, responsive frontends using React, Next.js, and Svelte, and craft scalable, high-performance backends with Node.js and Express. From intuitive user interfaces to rock-solid backend architecture, I ensure seamless, efficient, and reliable experiences across all platforms.
-                    <br/><br/>
-                    If you need a website that stands out, a mobile app that engages users, or a custom software solution tailored to your business, I’m here to help.
-                    <br/><br/>
-                    Let’s bring your vision to life. Reach out today!
-                </div>
-            </div> -->
         </div>
     </div>
     <div id="joshuaCont" class="flex lg:flex-row flex-col w-full md:justify-between sm:items-end items-center gap-20 overflow-hidden">
-        <div id="nameCont" class="w-full">
+        <div id="nameCont" class="flex flex-col w-full h-[180px] sm:h-[310px] justify-center">
             <div class="flex flex-col sm:text-start text-end sm:text-[100px] text-[60px] space-x-2 overflow-hidden">
                 <div class="animate-in slide-in-from-bottom duration-1200" class:md:hidden={$scrollY < 90} class:md:running={$scrollY > 300} class:md:paused={$scrollY <= 300}>
                     JOSHUA
@@ -66,7 +53,7 @@
             </div>
         </div>
         <div id="firstPhotoCont" class="flex">
-            <img class="object-contain rounded-4xl max-h-160 {$wiggle ? "animate-spring" : ""}" src="/images/experiences.jpg" alt="Joshua Lilienthal | Fullstack Engineer"/>
+            <img class="object-contain rounded-4xl max-h-160 {$wiggle ? "animate-spring" : ""}" src="/images/keyboard-wide.jpg" alt="Joshua Lilienthal | Fullstack Engineer"/>
         </div>
     </div>
     <div id="intersectionCont" class="flex flex-1 lg:flex-row flex-col w-full gap-4">        
@@ -81,8 +68,13 @@
             </div>
         </div>
     </div>
-    <div id="secondPhotoCont" class="flex lg:flex-row flex-col w-full justify-end items-center gap-20">
-        <div class="relative flex lg:flex-2 md:h-[700px] h-[400px] items-center w-full">
+    <div id="modernCont" class="flex lg:flex-row flex-col-reverse w-full justify-end items-center gap-20">
+        <div class="flex-1">
+            <div class="text-[30px] md:text-[38px] lg:text-[30px] 2xl:text-[36px]">
+                  MODERN FRONTEND FRAMEWORKS AND SCALABLE BACKEND SOLUTIONS&nbsp;— <br> DYNAMIC APPLICATIONS, INTUITIVE INTERFACES, AND ROBUST INFRASTRUCTURE. SPANNING REACT, NEXT.JS, SVELTE, NODE.JS, AND GRAPHQL—EVERY DIGITAL EXPERIENCE SHOULD BE BOTH BEAUTIFUL AND FUNCTIONAL.
+            </div>
+        </div>
+        <div id="secondPhotoCont" class="relative flex lg:flex-2 md:h-[700px] h-[400px] items-center w-full">
             <div class="absolute lg:top-10 top-4 lg:left-10 left-4">
                 <div class="flex flex-row + {lgTextClass} items-center">
                     WELCOME
@@ -111,11 +103,6 @@
                     alt="Joshua Lilienthal | Fullstack Engineer"
                 />
           </div>
-        </div>
-        <div class="flex-1">
-            <div class="text-[30px] md:text-[38px] lg:text-[30px] 2xl:text-[36px]">
-                  MODERN FRONTEND FRAMEWORKS AND SCALABLE BACKEND SOLUTIONS&nbsp;— <br> DYNAMIC APPLICATIONS, INTUITIVE INTERFACES, AND ROBUST INFRASTRUCTURE. SPANNING REACT, NEXT.JS, SVELTE, NODE.JS, AND GRAPHQL—EVERY DIGITAL EXPERIENCE SHOULD BE BOTH BEAUTIFUL AND FUNCTIONAL.
-            </div>
         </div>
     </div>
 </div>
