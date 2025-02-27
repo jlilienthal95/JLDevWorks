47e80d44eeae288e6a6623bad5d1bdc859accf24
joggest95 <jilienthal@gmail.com>
2025-02-26 20:21:07 -0500

    testing formatting for contact info

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -13,7 +13,7 @@
     })
 
     const lgTextClass = "text-[35px] sm:text-[60px] md:text-[70px] xl:text-[92px]"
-    const splitTextClass = "text-[65px] sm:text-[70px]"
+    const splitTextClass = "text-[55px] sm:text-[70px]"
 </script>
 
 <div id="bgCont" class="w-full h-screen absolute top-0 bg-black text-white" transition:slide={{ duration: 1000, easing: circIn}}>
@@ -21,7 +21,7 @@
         <div id="uiRowCont" class="flex flex-row w-full justify-end ">
             <div id="xCont" class="lg:pr-6 pt-10">
                 <a href="/" aria-label="home">
-                    <svg class="h-[10vh] lg:h-[15vh] md:h-[12vh]" xmlns="http://www.w3.org/2000/svg" width="20vh" height="20vh" viewBox="0 0 512 512" {...$$props}><path fill="currentColor" d="M256 48a208 208 0 1 1 0 416a208 208 0 1 1 0-416m0 464a256 256 0 1 0 0-512a256 256 0 1 0 0 512m-81-337c-9.4 9.4-9.4 24.6 0 33.9l47 47l-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47l47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47l47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47l-47-47c-9.4-9.4-24.6-9.4-33.9 0"/></svg>
+                    <svg class="lg:h-[15vh] md:h-[12vh] h-[8vh]" xmlns="http://www.w3.org/2000/svg" width="20vh" height="20vh" viewBox="0 0 512 512" {...$$props}><path fill="currentColor" d="M256 48a208 208 0 1 1 0 416a208 208 0 1 1 0-416m0 464a256 256 0 1 0 0-512a256 256 0 1 0 0 512m-81-337c-9.4 9.4-9.4 24.6 0 33.9l47 47l-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47l47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47l47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47l-47-47c-9.4-9.4-24.6-9.4-33.9 0"/></svg>
                 </a>
             </div>
         </div>
