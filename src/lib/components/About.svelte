4083ee32311a53b5195bcdd1efbccbe038c8c0c4
joggest95 <jilienthal@gmail.com>
2025-03-20 14:09:34 -0400

    changed to new svaria url for portfolio entry

--- src/lib/components/About.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { fade } from 'svelte/transition';
-</script>
-
-<div class="bg-black object-cover flex w-full h-screen justify-center items-center" transition:fade>
-    <div class="text-white">
-        About
-    </div>
-</div>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -3,7 +3,6 @@
 	import { onMount } from "svelte";
 	import { fade } from "svelte/transition";
     import { slide } from 'svelte/transition';
-    import { bounceIn, bounceOut } from 'svelte/easing';
 
     let delayDone = [false, false, false, false]; // Array to track each letter's delay
     
@@ -38,7 +37,7 @@
     <div id="portfolioContainer" class="flex flex-1 justify-center items-center w-full h-full xl:mt-[12vh] lg:mt-[6vh] lg:mb-0 mb-20 ">
         <div class="flex flex-col lg:flex-row lg:gap-24 gap-10 w-[90vw] h-full overflow-x-scroll overflow-y-visible justify-center lg:items-start items-center scroll-container">
             <Portfolio imgSrc="/portfolio/JBTravel.png" imgAlt="JB Travel" titleClass="text-black text-2xl" titleContent="JB Travel" descriptContent="JB Travel showcases a beautiful looping video, full screen scroll-snapping UI frames, and a robust SPA design utilizing Next.js and React." url="https://www.jbtravel.co"/>
-            <Portfolio imgSrc="/portfolio/SvariaHome.png" imgAlt="SvARIA" titleClass="text-black text-2xl" titleContent="SvARIA" descriptContent="More than just a website—it’s a mission to enhance the development of accessible apps through a powerful, ARIA-compliant component library for Svelte, built for speed and inclusivity." url="http://www.svaria.com"/>
+            <Portfolio imgSrc="/portfolio/SvariaHome.png" imgAlt="SvARIA" titleClass="text-black text-2xl" titleContent="SvARIA" descriptContent="More than just a website—it’s a mission to enhance the development of accessible apps through a powerful, ARIA-compliant component library for Svelte, built for speed and inclusivity." url="https://www.svaria.org"/>
         </div>
     </div>
 </div>
